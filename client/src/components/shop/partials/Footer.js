361a1dbf93c3fa4100ee74ea5a8cbac9be98a41b
Ehsanullah <mehsanjadoon786@gmail.com>
2026-01-15 19:59:05 +0500

    update:

--- client/src/components/shop/partials/Footer.js
+++ client/src/components/shop/partials/Footer.js
@@ -5,9 +5,9 @@ const Footer = (props) => {
 
   return (
     <Fragment>
-      <footer className="z-10 py-8 px-4 md:px-12 text-center glass-panel mt-auto border-t border-white/5">
-        <p className="text-gray-400 font-light tracking-wider">
-          <span className="font-bold text-brand-orange">BuyParts.PK</span> &copy; {moment().format("YYYY")} All Rights Reserved.
+      <footer className="z-10 py-2 px-4 md:px-12 text-center glass-panel mt-auto border-t border-white/5">
+        <p className="text-sm text-gray-400 font-light tracking-wider">
+          <span className="font-bold text-brand-orange">Buy Parts</span> &copy; {moment().format("YYYY")} All Rights Reserved.
         </p>
       </footer>
     </Fragment>
